48ee75c3f0040e1dde95c273410c7038ffb7870f
fsyed-cpp <fsyed@cpp.edu>
2023-04-02 17:11:44 -0700

    added more logs

--- kernel/Scheduler.cpp
+++ kernel/Scheduler.cpp
@@ -44,6 +44,7 @@ Scheduler::Result Scheduler::enqueue(Process *proc, bool ignoreState)
     } else if (proc->getPriority() == 3) {
         m_queue3.push(proc);
     } else if (proc->getPriority() == 4) {
+        ERROR("Pushing process " << proc->getID() << " to priority 4 queue");
         m_queue4.push(proc);
     } else if (proc->getPriority() == 5) {
         m_queue5.push(proc);
